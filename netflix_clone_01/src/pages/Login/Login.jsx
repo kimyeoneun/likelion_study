9ad3774b240ac2512e57b47df42a5baeedf7740b
kimyeoneun <kye00150@naver.com>
2025-05-18 01:53:55 +0900

    Add files via upload

---
+++ netflix_clone_01/src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+import logo from '../../assets/logo.png'
+
+const Login = () => {
+  return (
+    <div className='login'>
+    </div>
+  )
+}
+
+export default Login
